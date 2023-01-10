dcd9585e459c18cce8e06c271377d224b5657c97
Tom Kieronski <tomasz@gemom.com.au>
2023-01-10 18:31:50 +0800

    magic ball flutter app

---
+++ android/app/src/main/kotlin/au/com/genom/magicball/magicball/MainActivity.kt
@@ -0,0 +1,6 @@
+package au.com.genom.magicball.magicball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _ballNumber = 1;
+
+  void _incrementCounter() {
+    setState(() {
+      _ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        color: Colors.blue,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                style: ButtonStyle(
+                  elevation: MaterialStateProperty.all(0.0),
+                ),
+                onPressed: _incrementCounter,
+                child: Image.asset('images/ball$_ballNumber.png'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
